f9fa317a5dd55dc160419e033497df9ede594ed2
Aadit Garg <69139031+Aadit-Garg@users.noreply.github.com>
2026-03-29 19:58:26 +0530

    screen fast

--- main.py
+++ main.py
@@ -6,8 +6,29 @@ from vars import *
 pygame.init()
 screen = pygame.display.set_mode(vars.screen_size)
 running = True
-pixel_size = m.Vector2(screen_size.x / grid_size.x, screen_size.y / grid_size.y)
 
 FPS = 60
 clock = pygame.time.Clock()
 
+
+def generate_space():
+    base = np.random.randint(0,256, grid_size, dtype=np.uint8)
+    rgb_array = np.stack((base,base,base), axis = -1)
+
+    small_surf = pygame.surfarray.make_surface(rgb_array)
+    return pygame.transform.scale(small_surf, screen_size, screen)
+
+while running:
+
+    for event in pygame.event.get():
+    
+        if event.type == pygame.QUIT:
+            running = False
+    
+        elif event.type == pygame.KEYDOWN:
+            generate_space()
+
+    pygame.display.flip()
+
+
+pygame.quit()
--- vars/__init__.py
+++ vars/__init__.py
@@ -1,3 +1,3 @@
 import pygame.math as m
-grid_size = m.Vector2(64,64)
+grid_size = (64,64)
 screen_size = m.Vector2(640, 640)
